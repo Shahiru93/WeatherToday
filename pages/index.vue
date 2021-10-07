0e9a9742f2c37baa284fdb59ebd92c3ba2fea787
Shahiru Roshen <68189195+Shahiru93@users.noreply.github.com>
2021-10-07 16:42:56 +0530

    fetch weather data

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <v-row></v-row>
+  <v-container fluid>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        city: "London"
+      }
+    },
+    created(){
+      this.$axios.$get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&129c5d65810fd42820964e446af9b61f`).then(res => console.log(res))
+    }
+  }
+</script>
